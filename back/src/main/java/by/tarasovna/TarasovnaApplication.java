d1233f7fd498011a86d23944d35d1272e12d9fa6
Yahor.Pivavarchyk <yahor.pivavarchyk@nces.by>
2026-03-18 13:00:52 +0300

    first commit

---
+++ back/src/main/java/by/tarasovna/TarasovnaApplication.java
@@ -0,0 +1,13 @@
+package by.tarasovna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarasovnaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TarasovnaApplication.class, args);
+    }
+
+}
---
+++ back/src/test/java/by/tarasovna/TarasovnaApplicationTests.java
@@ -0,0 +1,13 @@
+package by.tarasovna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarasovnaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
